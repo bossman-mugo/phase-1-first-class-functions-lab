078adf3a8fda4e006c6037dfe50a440a8e7964aa
Eric <mugoericpg20@gmail.com>
2023-03-19 15:26:43 +0300

    Stuck on fare price

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function([drivers]){
+    drivers = ["Antonia","Nuru"]
+    const newArray = drivers.slice(0,2);
+    return newArray;
+}
+
+const returnLastTwoDrivers = function([drivers]){
+    drivers = ["Antonia","Nuru","Amari","Mo"]
+    const newArray = drivers.slice(-2);
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(number){
+    return function farePrice(fare){
+        let total;
+        total = fare * number;
+
+
+    }
+
+}
+
+const fareDoubler = function(){
+    function farePrice(far)
+
+    return createFareMultiplier(farePrice);
+
+
+}
+
